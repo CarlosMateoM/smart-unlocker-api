adaf0c87d0188d678eded56837b7627ff45f754b
CarlosMateoM <carlosmateo484@gmail.com>
2024-11-19 14:20:58 -0500

    implementacion funcional

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'role' => 'admin',
+            'name' => 'Admin',
+            'email' => 'admin@correo.com',
+            'password' => bcrypt('admin1234'),
         ]);
     }
 }
